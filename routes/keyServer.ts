585691d73183efb54883c26cbb41929b0add57b2
PrivateAS <98113938+Private-AS@users.noreply.github.com>
2026-06-28 00:55:18 +0200

    SAST: fix SQLi (search, login) + path traversal (keyServer), filter test/codefix FPs

--- routes/keyServer.ts
+++ routes/keyServer.ts
@@ -9,9 +9,14 @@ import { type Request, type Response, type NextFunction } from 'express'
 export function serveKeyFiles () {
   return ({ params }: Request, res: Response, next: NextFunction) => {
     const file = params.file
-
     if (!file.includes('/')) {
-      res.sendFile(path.resolve('encryptionkeys/', file))
+      const basePath = path.resolve('encryptionkeys/')
+      const target = path.resolve(basePath, file)
+      if (!target.startsWith(basePath + path.sep)) {
+        res.status(403)
+        return next(new Error('Invalid file path!'))
+    }
+    res.sendFile(target)
     } else {
       res.status(403)
       next(new Error('File names cannot contain forward slashes!'))
