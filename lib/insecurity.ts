fb351a1a6f6760e2868278c1d60272aedceef002
PrivateAS <98113938+Private-AS@users.noreply.github.com>
2026-06-28 14:31:39 +0200

    Secret scanning: added placeholder value for private key to use the same convention as public key in lib/insecurity.ts

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -18,7 +18,7 @@ import * as utils from './utils'
 import * as z85 from 'z85'
 
 export const publicKey = fs ? fs.readFileSync('encryptionkeys/jwt.pub', 'utf8') : 'placeholder-public-key'
-const privateKey = fs.readFileSync('encryptionkeys/jwt.priv', 'utf8')
+const privateKey = fs ? fs.readFileSync('encryptionkeys/jwt.priv', 'utf8') : 'placeholder-private-key'
 
 interface ResponseWithUser {
   status?: string
